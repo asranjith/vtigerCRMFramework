11590751ea084b48a887ab4017a478f26ab2ba3b
vino <vino@User.mshome.net>
2024-11-06 15:52:03 +0530

    UPDATED

--- Selenium_VtigerCRM_Framedummy/src/main/java/com/comcast/crm/objectRepository/utility/TroubleTickets.java
+++ Selenium_VtigerCRM_Framedummy/src/main/java/com/comcast/crm/objectRepository/utility/TroubleTickets.java
@@ -1,5 +1,4 @@
-package com.CRM.ObjectRepository;
-
+package com.comcast.crm.objectRepository.utility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
