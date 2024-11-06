95da5916cb2ef30044e3061f9aa49db7a08a7ddf
asranjith <rockyranjith27@gmail.com>
2024-11-06 11:07:00 +0530

    home page modification

--- Selenium_VtigerCRM_Framedummy/src/main/java/com/comcast/crm/objectRepository/utility/HomePage.java
+++ Selenium_VtigerCRM_Framedummy/src/main/java/com/comcast/crm/objectRepository/utility/HomePage.java
@@ -6,38 +6,46 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-     public HomePage(WebDriver driver) {
-    	 PageFactory.initElements(driver, this);
-     }
-     @FindBy(xpath = "//a[text()='Calendar']")
-     private WebElement calendarHeader;
-     
-     @FindBy(xpath = "//a[text()='Leads']")
-     private WebElement leadsHeader;
-     
-     @FindBy(xpath = "//a[text()='Organizations']")
-     private WebElement organizationsHeader;
-     
-     @FindBy(xpath = "//a[text()='Contacts']")
-     private WebElement contactsHeader;
-     
-     @FindBy(xpath = "//a[text()='Opportunities']")
-     private WebElement opportunitiesHeader;
-     
-     @FindBy(xpath = "//a[text()='Products']")
-     private WebElement productsHeader;
-     
-     @FindBy(xpath = "//a[text()='Documents']")
-     private WebElement documentsHeader;
-     
-     @FindBy(xpath = "//a[text()='Email']")
-     private WebElement emailHeader;
-     
-     @FindBy(xpath = "//a[text()='Trouble Tickets']")
-     private WebElement troubleticketsHeader;
-     
-     @FindBy(xpath = "//a[text()='Dashboard']")
-     private WebElement dashboardHeader;
+	public HomePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//a[text()='More']")
+	private WebElement moreHeader;
+
+	public WebElement getMoreHeader() {
+		return moreHeader;
+	}
+
+	@FindBy(xpath = "//a[text()='Calendar']")
+	private WebElement calendarHeader;
+
+	@FindBy(xpath = "//a[text()='Leads']")
+	private WebElement leadsHeader;
+
+	@FindBy(xpath = "//a[text()='Organizations']")
+	private WebElement organizationsHeader;
+
+	@FindBy(xpath = "//a[text()='Contacts']")
+	private WebElement contactsHeader;
+
+	@FindBy(xpath = "//a[text()='Opportunities']")
+	private WebElement opportunitiesHeader;
+
+	@FindBy(xpath = "//a[text()='Products']")
+	private WebElement productsHeader;
+
+	@FindBy(xpath = "//a[text()='Documents']")
+	private WebElement documentsHeader;
+
+	@FindBy(xpath = "//a[text()='Email']")
+	private WebElement emailHeader;
+
+	@FindBy(xpath = "//a[text()='Trouble Tickets']")
+	private WebElement troubleticketsHeader;
+
+	@FindBy(xpath = "//a[text()='Dashboard']")
+	private WebElement dashboardHeader;
 
 	public WebElement getCalendarHeader() {
 		return calendarHeader;
@@ -79,26 +87,23 @@ public class HomePage {
 		return dashboardHeader;
 	}
 
-    @FindBy(xpath = "//span[text()=' Administrator']/parent::td/following-sibling::td/img[contains(@src,'user')]")
-    private WebElement usericon;
-    
-    public WebElement getUsericon() {
+	@FindBy(xpath = "//span[text()=' Administrator']/parent::td/following-sibling::td/img[contains(@src,'user')]")
+	private WebElement usericon;
+
+	public WebElement getUsericon() {
 		return usericon;
 	}
-
 
 	public WebElement getSignOutLink() {
 		return signOutLink;
 	}
 
 	@FindBy(xpath = "//a[text()='Sign Out']")
-    private WebElement signOutLink;
-    
-    
-    public void signOut() {
-    	usericon.click();
-    	signOutLink.click();
-    }
-     
-     
+	private WebElement signOutLink;
+
+	public void signOut() {
+		usericon.click();
+		signOutLink.click();
+	}
+
 }
